2ee5a5b1bb8859616777235771f15d936f9376bd
melnee <melanie.neilan@gmail.com>
2021-04-04 10:39:53 -0700

    create message action, backend and frontend

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,18 @@
 class MessagesController < ApplicationController
+    before_action :require_user
+
+    def create
+        #the build method creates a message with params and inserts into messages array for user
+        message = current_user.messages.build(message_params)
+        if message.save 
+            redirect_to root_path
+        end
+    end
+
+    private
+
+    def message_params
+        params.require(:message).permit(:body)
+    end
+
 end
